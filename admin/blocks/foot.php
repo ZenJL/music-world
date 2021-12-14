c29e13a5c3217b3f9a4b49adf8d6e279b14b7d6d
thuthao9623 <95998105+thuthao9623@users.noreply.github.com>
2021-12-14 23:57:14 +0700

    Update foot.php

--- admin/blocks/foot.php
+++ admin/blocks/foot.php
@@ -5,3 +5,26 @@
 <script src="public/dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="public/dist/js/demo.js"></script>
+
+</script?
+    <!-- AdminLTE for demo purposes -->
+    <
+    script src = "public/dist/js/demo.js" > </script?>
+
+    <
+    script type = "text/Javascript" >
+        $(document).ready(function() {
+            bsCustomFileInput.init();
+
+            $("#datatable").DataTable({
+                "responsive": true,
+                "autowidth": false,
+            });
+        });
+
+    function imgError(image) {
+        image.onerror = "";
+        image.src = "../public/image/avatar.png";
+        return true;
+    } <
+    /script
