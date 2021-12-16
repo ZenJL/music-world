3a1535b56f627f00df1fcd5bafcddcc5b236ea0f
ZenJL <minhle7206@gmail.com>
2021-12-16 22:13:48 +0700

    update product & user

--- admin/model/user.php
+++ admin/model/user.php
@@ -1,70 +1,72 @@
-<?php
-
-function check_user_exist($conn, $data, $edit = false) {
+<?php 
+function check_user_exist ($conn,$data,$edit = false) {
     if (!$edit) {
         $stmt = $conn->prepare("SELECT email FROM user WHERE email = :email");
-    }
-    else {
+    } else {
         $stmt = $conn->prepare("SELECT email FROM user WHERE email = :email AND id != :id");
-        $stmt->bindParam(':id', $data["id"], PDO::PARAM_STR);
+        $stmt->bindParam(':id',$data["id"],PDO::PARAM_STR);
     }
-    $stmt->bindParam(':email', $data["email"], PDO::PARAM_STR);
+    
+    $stmt->bindParam(':email',$data["email"],PDO::PARAM_STR);
     $stmt->execute();
     $count = $stmt->rowCount();
-    if ($count > 0){
+
+    if ($count > 0) {
         return false;
     }
 
     return true;
 }
-function create_user($conn, $user) {
-    $stmt = $conn->prepare("INSERT INTO user (email, password, level) VALUES (:email, :password, :level)");
-    $stmt->bindParam(':email', $user["email"], PDO::PARAM_STR);
-    $stmt->bindParam(':password', $user["password"], PDO::PARAM_STR);
-    $stmt->bindParam(':level', $user["level"], PDO::PARAM_INT);
+
+function create_user ($conn,$user) {
+    $stmt = $conn->prepare("INSERT INTO user (email,password,level) VALUES (:email,:password,:level)");
+    $stmt->bindParam(':email',$user["email"],PDO::PARAM_STR);
+    $stmt->bindParam(':password',$user["password"],PDO::PARAM_STR);
+    $stmt->bindParam(':level',$user["level"],PDO::PARAM_INT);
     $stmt->execute();
     return $stmt;
 }
-function get_all_user ($conn){
-    $stmt =$conn->prepare("SELECT * FROM user ORDER BY id DESC");
+
+function get_all_user ($conn) {
+    $stmt = $conn->prepare("SELECT * FROM user ORDER BY id DESC");
     $stmt->execute();
     $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
     return $data;
 }
 
-function get_user ($conn,$id){
-    $stmt =$conn->prepare("SELECT * FROM user where id = :id");
+function get_user ($conn,$id) {
+    $stmt = $conn->prepare("SELECT * FROM user WHERE id = :id");
     $stmt->bindParam(":id",$id,PDO::PARAM_STR);
     $stmt->execute();
-    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $data = $stmt->fetch(PDO::FETCH_ASSOC);
     return $data;
 }
 
-
-function check_user_id ($conn, $id) {
-    $stmt = $conn->prepare("SELECT * FROM user  WHERE id = :id");
-    $stmt->bindParam(":id", $id,PDO::PARAM_STR);
+function check_user_id ($conn,$id) {
+    $stmt = $conn->prepare("SELECT * FROM user WHERE id = :id");
+    $stmt->bindParam(":id",$id,PDO::PARAM_STR);
     $stmt->execute();
     $count = $stmt->rowCount();
 
-    if($count >0){
-        return True;
+    if ($count > 0) {
+        return true;
     }
+
     return false;
 }
 
-function edit_user($conn, $user){
-    $stmt = $conn->prepare("UPDATE user SET password =:password, level = :level, id = :id");
-    $stmt->bindParam(':password', $user["password"], PDO::PARAM_STR);
-    $stmt->bindParam(':level', $user["level"], PDO::PARAM_INT);
-    $stmt->bindParam(':id', $user["id"], PDO::PARAM_INT);
+function edit_user ($conn,$user) {
+    $stmt = $conn->prepare("UPDATE user SET password = :password, level = :level WHERE id = :id");
+    $stmt->bindParam(':password',$user["password"],PDO::PARAM_STR);
+    $stmt->bindParam(':level',$user["level"],PDO::PARAM_INT);
+    $stmt->bindParam(':id',$user["id"],PDO::PARAM_INT);
     $stmt->execute();
     return $stmt;
 }
 
-function delete_user($conn, $id){
-    $stmt = $conn->prepare("DELETE FROM  user where id = :id");
-    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+function delete_user ($conn,$id) {
+    $stmt = $conn->prepare("DELETE FROM user WHERE id = :id");
+    $stmt->bindParam(':id',$id,PDO::PARAM_INT);
     $stmt->execute();
     return $stmt;
 }
