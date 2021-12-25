40be583fb9d12884ba49e69cbd53a9ceef283ab2
Nam Nguyen <93068424+afacade@users.noreply.github.com>
2021-12-25 21:09:30 +0700

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 // database config
 const DBHOST = "localhost";
 const DBNAME = "music_world";
-const DBUSER = "superuser";
-const DBPASS = "denho123";
+const DBUSER = "root";
+const DBPASS = "";
 const DBUTF8 = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
 ?>
